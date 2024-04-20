e392c67447166527fa39a6797040619440f2f5c1
Myniuu <michalszczygiel96@gmail.com>
2024-04-20 13:26:54 +0200

    add logic for shuffle game cards

--- Memory card/src/components/GameArea/GameArea.jsx
+++ Memory card/src/components/GameArea/GameArea.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 import { Button } from "../Button/Button";
-import { cardStash } from "../CardStash/CardStash";
 import styles from "./GameArea.module.css";
 
 export function GameArea() {
-  const [changePosition, setChangePosition] = useState("");
+  const [cardStash, setCardStash] = useState([
+    { id: 1, cardName: "🍎" },
+    { id: 2, cardName: "🍐" },
+    { id: 3, cardName: "🍌" },
+    { id: 4, cardName: "🍉" },
+    { id: 5, cardName: "🍓" },
+    { id: 6, cardName: "🥑" },
+    { id: 7, cardName: "🍇" },
+    { id: 8, cardName: "🍋" },
+    { id: 9, cardName: "🫐" },
+    { id: 10, cardName: "🍊" },
+    { id: 11, cardName: "🥭" },
+    { id: 12, cardName: "🍍" },
+  ]);
+
+  function shuffle() {
+    const shuffledStash = cardStash.slice();
+    return shuffledStash.sort(() => Math.floor(Math.random() - 0.5));
+  }
+
+  function handleButtonClick() {
+    const shuffledStash = shuffle();
+    setCardStash(shuffledStash);
+  }
 
   return (
     <main>
       {cardStash.map((card) => (
-        <Button
-          value={card.id}
-          onClick={(e) => console.log(e.target.value)}
-          key={card.id}
-        >
+        <Button value={card.id} onClick={handleButtonClick} key={card.id}>
           {card.cardName}
         </Button>
       ))}
